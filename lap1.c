00a241eefaf91f582eacf32f26403bfed0c06c21
6704062611166 <s6704062611166@email.kmutnb.ac.th>
2024-11-28 14:33:57 +0700

    add input

--- lap1.c
+++ lap1.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
 int main() {
-	int num1=5;
-	int num2=10;
-	if (num1>num2) {
+	int myNum1;
+	printf("Type a number and press enter: \n"); 
+	scanf("%d", &myNum1);
+	int myNum2;
+	printf("Type a number and press enter: \n");
+	scanf("%d", &myNum2); 
+	if (myNum1>myNum2) {
 		printf("num1 greater then num2");
 	} else {
 		printf("num2 greater then num1");
