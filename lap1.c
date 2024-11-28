7d4165d364d384647dd745f46a7436ebad8320e7
6704062611166 <s6704062611166@email.kmutnb.ac.th>
2024-11-28 14:16:14 +0700

    work 1

---
+++ lap1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+	int num1=5;
+	int num2=10;
+	if (num1>num2) {
+		printf("num1 greater then num2");
+	}
+	if (num2>num1) {
+		printf("num2 greater then num1");
+	}
+}
