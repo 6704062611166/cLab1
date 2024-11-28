f8026c26ff147b997f36c9304f2880f4931aa2c2
6704062611166 <s6704062611166@email.kmutnb.ac.th>
2024-11-28 14:43:48 +0700

    input multi x2

--- lap1.c
+++ lap1.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main() {
 	int myNum1;
+	scanf("%d", &myNum1 );
 	int myNum2;
-	printf("Type a number and press enter: \n"); 
-	scanf("%d %d", &myNum1, &myNum2 );
+	scanf("%d", &myNum2 );
 	if (myNum1>myNum2) {
 		printf("num1 greater then num2");
 	} else {
